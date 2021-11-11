994e84d96ab5ef254a4c3c25d41a086f247d4e91
gresm <>
2021-11-11 14:08:22 +0100

    + area.py(Shape)

--- DataStructure/Tools/area.py
+++ DataStructure/Tools/area.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from typing import Sequence
+
 from . import Position
 
 
@@ -15,3 +19,51 @@ class Rect:
 
     def on_border(self, pos: Position):
         return not (self.in_range(pos) or self.out_range(pos))
+
+
+class Shape:
+    def __init__(self, rect: Rect | Shape, *r: Rect | Shape, exclude: Sequence[Rect | Shape] = ()):
+        self.rects: list[Rect | Shape] = [rect] + list(r)
+        self.exclude: list[Rect | Shape] = list(exclude)
+
+    def in_range(self, pos: Position) -> bool:
+        mach = False
+        for r in self.rects:
+            if r.in_range(pos):
+                mach = True
+                break
+
+        if mach:
+            for e in self.exclude:
+                if e.in_range(pos):
+                    return False
+            return True
+        return False
+
+    def out_range(self, pos: Position) -> bool:
+        mach = False
+        for r in self.rects:
+            if r.out_range(pos):
+                mach = True
+                break
+
+        if mach:
+            for e in self.exclude:
+                if e.out_range(pos):
+                    return False
+            return True
+        return False
+
+    def on_border(self, pos: Position) -> bool:
+        mach = False
+        for r in self.rects:
+            if r.on_border(pos):
+                mach = True
+                break
+
+        if mach:
+            for e in self.exclude:
+                if e.on_border(pos):
+                    return False
+            return True
+        return False
