42f7efa9a4aea12559855e8a680b2df91347a8dd
gresm <78505251+gresm@users.noreply.github.com>
2022-02-14 21:40:59 +0100

    ~ data_structure/ ~ abstract_structures.py

--- data_structure/abstract_structures.py
+++ data_structure/abstract_structures.py
@@ -7,6 +7,7 @@ class ClanStats:
     def __init__(self):
         self.herbs = 0
         self.prey = 0
+        self.coins = 0
 
 
 class Clan:
@@ -14,6 +15,25 @@ class Clan:
         self.name = name
         self.stats = ClanStats()
         self.cats: set[Entity] = set()
+
+
+class Game:
+    def __init__(self, player1: str, *players: str):
+        self._player_names = (player1, ) + players
+        self.players = {name: Clan(name) for name in self._player_names}
+        self.__player_names_iter = iter(self._player_names)
+        self._current_player: str = ...
+
+    @property
+    def current_player(self):
+        return self.players[self._current_player]
+
+    def next_player(self):
+        try:
+            self._current_player = next(self.__player_names_iter)
+        except StopIteration:
+            self.__player_names_iter = iter(self._player_names)
+            self.next_player()
 
 
 __all__ = [
