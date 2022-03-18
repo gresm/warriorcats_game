afdc0d5037820afc5c6febfb2a926b52151d8263
gresm <78505251+gresm@users.noreply.github.com>
2022-03-18 12:00:00 +0100

    ~ assets/sprite_sheet/ ~ sprite_sheet_editor.py - Moving and Scaling

--- assets/sprite_sheet/sprite_sheet_editor.py
+++ assets/sprite_sheet/sprite_sheet_editor.py
@@ -1,6 +1,7 @@
 """
 Editor for sprite sheets
 """
+from __future__ import annotations
 
 import sys
 import json
@@ -38,6 +39,21 @@ fps = 60
 clock = pg.time.Clock()
 
 
+# Define extra variables
+current_selection: pg.Rect | None = None
+start_pressing = pg.Vector2(0, 0)
+image_offset = pg.Vector2(0, 0)
+zoom = 1
+zoom_down_speed = 0.9
+zoom_up_speed = 1/zoom_down_speed
+cached_image = image.copy()
+
+
+# Define some constants
+RIGHT = pg.Vector2(1, 0)
+DOWN = pg.Vector2(0, 1)
+
+
 # Mainloop
 while running:
     # Events
@@ -50,7 +66,10 @@ while running:
         elif event.type == pg.MOUSEBUTTONDOWN:
             if event.button == 1:
                 # Left click
-                pass
+                start_pressing = pg.Vector2(event.pos)
+                # If we have a current selection, we are removing it
+                if current_selection:
+                    current_selection = None
             elif event.button == 2:
                 # Middle click
                 pass
@@ -58,8 +77,101 @@ while running:
                 # Right click
                 pass
 
+    # Read currently pressed mouse keys
+    mouse_pressed = pg.mouse.get_pressed()
+    mouse_pos = pg.mouse.get_pos()
+
+    if mouse_pressed[0]:
+        # Left click
+        if current_selection is None:
+            current_selection = pg.Rect(start_pressing, pg.Vector2(0, 0))
+        else:
+            current_selection.size = pg.Vector2(mouse_pos) - start_pressing
+
+    # Read currently pressed keys
+    keys_pressed = pg.key.get_pressed()
+
+    # Selection manipulation
+
+    if keys_pressed[pg.K_LEFT]:
+        current_selection.size -= RIGHT
+    if keys_pressed[pg.K_RIGHT]:
+        current_selection.size += RIGHT
+    if keys_pressed[pg.K_UP]:
+        current_selection.size -= DOWN
+    if keys_pressed[pg.K_DOWN]:
+        current_selection.size += DOWN
+    if keys_pressed[pg.K_w]:
+        current_selection.topleft -= DOWN
+    if keys_pressed[pg.K_s]:
+        current_selection.topleft += DOWN
+    if keys_pressed[pg.K_a]:
+        current_selection.topleft -= RIGHT
+    if keys_pressed[pg.K_d]:
+        current_selection.topleft += RIGHT
+    if keys_pressed[pg.K_i]:
+        current_selection.size += DOWN
+        current_selection.topleft -= DOWN
+    if keys_pressed[pg.K_k]:
+        current_selection.size -= DOWN
+        current_selection.topleft += DOWN
+    if keys_pressed[pg.K_j]:
+        current_selection.size += RIGHT
+        current_selection.topleft -= RIGHT
+    if keys_pressed[pg.K_l]:
+        current_selection.size -= RIGHT
+        current_selection.topleft += RIGHT
+    if keys_pressed[pg.K_DELETE]:
+        current_selection = None
+    if keys_pressed[pg.K_BACKSPACE]:
+        current_selection = None
+
+    reload_cached_image = False
+
+    # Zoom
+    if keys_pressed[pg.K_EQUALS]:
+        zoom *= zoom_up_speed
+        reload_cached_image = True
+    if keys_pressed[pg.K_MINUS]:
+        zoom *= zoom_down_speed
+        reload_cached_image = True
+    if keys_pressed[pg.K_0]:
+        zoom = 1
+        reload_cached_image = True
+
+    # Update image offset with t, g, f, h
+    if keys_pressed[pg.K_t]:
+        image_offset -= DOWN
+    if keys_pressed[pg.K_g]:
+        image_offset += DOWN
+    if keys_pressed[pg.K_f]:
+        image_offset -= RIGHT
+    if keys_pressed[pg.K_h]:
+        image_offset += RIGHT
+
+    # Additional zoom keys with r, y
+    if keys_pressed[pg.K_r]:
+        zoom *= zoom_up_speed
+        reload_cached_image = True
+    if keys_pressed[pg.K_y]:
+        zoom *= zoom_down_speed
+        reload_cached_image = True
+
+    # Reload cached image, if needed
+    if reload_cached_image:
+        if zoom == 0:
+            zoom = 0.00001
+        cached_image = pg.transform.scale(image, (int(image.get_width() * zoom), int(image.get_height() * zoom)))
+
+    # Clear window
+    window.fill((0, 0, 0))
+
     # Draw the image
-    window.blit(image, (0, 0))
+    window.blit(cached_image, image_offset)
+    if current_selection:
+        pg.draw.rect(window, (255, 0, 0), current_selection, 1)
+
+    # Update the display
     pg.display.update()
 
     # Update the clock
